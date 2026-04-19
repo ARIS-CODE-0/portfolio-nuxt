3b5b224742b76e0c6729750b7cca222d532e4282
Aris <aris@example.com>
2026-04-19 23:22:49 +0000

    fix: move app.vue to app/ directory for Nuxt 4 compatibility

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-mocha-light min-h-screen">
+    <AppHeader />
+    <HeroSection />
+    <StackSection />
+    <ProjectSection />
+    <ContactSection />
+    <AppFooter />
   </div>
 </template>
+
+<script setup>
+// Nuxt auto-imports components from /components
+</script>
